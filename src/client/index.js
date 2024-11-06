7ad42a7e5def33a2ca0940383ea584b6508246a7
Sofia ViP <up202108682@up.pt>
2024-11-06 20:34:29 +0000

    Added main page

--- src/client/index.js
+++ src/client/index.js
@@ -7,8 +7,19 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+app.use(express.static('public'));
+
+// Routes
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+  });
+
+app.get('/register', (req, res) => {
+res.render('register');
 });
 
 const port = process.argv[2] || 3000;
