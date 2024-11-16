325937ac88d0a4b394b4231742f49e18e601d284
Sofia ViP <up202108682@up.pt>
2024-11-16 01:00:22 +0000

    Partially added delete button

--- src/client/public/js/app.js
+++ src/client/public/js/app.js
@@ -4,11 +4,37 @@ function addEventListeners() {
     [].forEach.call(copyListBtn, function(btn) {
         btn.addEventListener('click', copyList);
     });
+
+    let deleteListBtn = document.querySelectorAll('.del-btn');
+    [].forEach.call(deleteListBtn, function(btn) {
+        btn.addEventListener('click', confirmDelete);
+    });
 }
 
 function copyList(event) {
     const listId = event.target.closest('li').getAttribute('data-id');
     navigator.clipboard.writeText(listId);
+}
+
+function confirmDelete (event){
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#304700",
+      cancelButtonColor: "#C2C2C2",
+      confirmButtonText: "Yes"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteList(event);
+      }
+    });
+}
+
+function deleteList(event) {
+    const listId = event.target.closest('li').getAttribute('data-id');
+    console.log("Deleting list with id: " + listId);
 }
 
 addEventListeners();
